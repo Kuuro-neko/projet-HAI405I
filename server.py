c03114fb999fb2a69ba589b7d9a471484e810286
Gilles Gonzalez Oropeza <95430715+Kuuro-neko@users.noreply.github.com>
2023-01-16 12:48:31 +0000

    Login et logout

--- server.py
+++ server.py
@@ -3,12 +3,13 @@ from flask import render_template
 from flask import request
 from flask import make_response
 
-from flask import Flask, redirect, url_for, request
+from flask import redirect, url_for, request, session
 
 import json
 
 
 app = Flask(__name__)
+app.secret_key = "super secret key"
 
 host = '0.0.0.0'
 port = 8888
@@ -16,7 +17,15 @@ port = 8888
 @app.route("/")
 @app.route("/index")
 def index():
-   return render_template("index.html")
+   if 'user' in session:
+      name = session['user']
+      return render_template("index.html", name = name)
+   return render_template("index.html", name = None)
+
+@app.route("/logout")
+def logout():
+   session.pop('user', None)
+   return redirect(url_for('index'))
 
 @app.route("/login", methods = ['POST', 'GET'])
 def login():
@@ -27,9 +36,8 @@ def login():
       password = request.form['password']
       for users in data:
          if users['user'] == login and users['password'] == password:
-            resp = make_response(render_template("index.html", user = login))
-            resp.set_cookie('user', login)
-            return resp
+            session['user'] = login
+            return render_template("index.html")
       return render_template("login.html", error = "Login ou mot de passe incorrect")
    else:
       return render_template("login.html")
